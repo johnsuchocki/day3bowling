efc946c6192c60072f8f28f61d41e7f962e387ff
John <jsuchocki@gmail.com>
2016-08-03 19:43:04 -0400

    8/3/16 7:42p EST day 3 lab update -added some comments

--- src/main/java/day3lab/LetsGoBowling.java
+++ src/main/java/day3lab/LetsGoBowling.java
@@ -66,33 +66,34 @@ public class LetsGoBowling {
 			if (framex == 10) {
 				if ((totalThrow[18] == 10) || (totalThrow[18] + totalThrow[19] == 10)) {
 					totalFrame[9] = totalThrow[18] + totalThrow[19] + totalThrow[20];
-					//System.out.println("Frame 10 score was " + totalFrame[9]);
+					System.out.println("Frame 10 score was " + totalFrame[9]);
 				} else {
 					totalFrame[9] = totalThrow[18] + totalThrow[19];
-					//System.out.println("Frame 10 score was " + totalFrame[9]);
+					System.out.println("Frame 10 score was " + totalFrame[9]);
 				}
+			//logic for frames 1-9
 			} else {
 				//checks for strikes
 				if (totalThrow[framex * 2 - 2] == 10) {
 					//checks for double strikes
 					if (totalThrow[framex * 2] == 10) {
 						totalFrame[framex] = totalThrow[framex * 2 - 2] + totalThrow[framex * 2] + totalThrow[framex * 2 + 2];
-						//System.out.println("Frame " + framex + " score was " + totalFrame[framex]);
+						System.out.println("Frame " + framex + " score was " + totalFrame[framex]);
 					//handles strike then non-strike
 					} else {
 						totalFrame[framex] = totalThrow[framex * 2 - 2] + totalThrow[framex * 2] + totalThrow[framex * 2 + 1];
-						//System.out.println("Frame " + framex + " score was " + totalFrame[framex]);
+						System.out.println("Frame " + framex + " score was " + totalFrame[framex]);
 					}
 					
 				//checks for spares
 				} else if (totalThrow[framex * 2 - 2] + totalThrow[framex * 2 - 1] == 10) {
 					totalFrame[framex] = totalThrow[framex * 2 - 2] + totalThrow[framex * 2 - 1] + totalThrow[framex * 2];
-					//System.out.println("Frame " + framex + " score was " + totalFrame[framex]);
+					System.out.println("Frame " + framex + " score was " + totalFrame[framex]);
 				
 					//handles all non-strikes/spares
 				} else {
 					totalFrame[framex] = totalThrow[framex * 2 - 2] + totalThrow[framex * 2 - 1];
-					//System.out.println("Frame " + framex + " score was " + totalFrame[framex]);
+					System.out.println("Frame " + framex + " score was " + totalFrame[framex]);
 				}
 			}
 		}
